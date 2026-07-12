4031c550e57be79c6a527b1fd3a4233b9559fa83
Joshua Foster <josh2112@gmail.com>
2026-07-12 19:37:36 -0400

    a bit more styling

--- ui/src/components/GoalView.vue
+++ ui/src/components/GoalView.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { toGoalStats, type Goal } from '../models/goal';
-import { useSession } from '../stores/session';
-import { formatDec } from '../utils';
+import { toGoalStats, type Goal } from "../models/goal";
+import { useSession } from "../stores/session";
+import { formatDec } from "../utils";
 
 const props = defineProps<{
-    goal: Goal
+    goal: Goal;
 }>();
 
 const session = useSession();
@@ -12,14 +12,17 @@ const stats = toGoalStats(props.goal, session.settings!.distance_unit);
 </script>
 
 <template>
-    <li>
-        {{ stats.name }}<br>
-        {{ formatDec(stats.total_dist, 2) }} {{ stats.dist_abbr }} in {{ stats.total_days }} days
-        <br>
-        <progress :value="stats.goal.current_distance_meters" :max="stats.goal.distance_meters" /><br>
-        {{ formatDec(stats.current_dist, 2) }} {{ stats.dist_abbr }} / {{ formatDec(stats.percent, 0) }}% complete
-        ({{ formatDec(Math.abs(stats.current_pace_diff), 2) }} {{ stats.current_pace_diff >= 0 ? 'over' : 'under' }}
-        pace)<br>
-        {{ stats.remaining_days }} days to go ({{ formatDec(stats.remaining_pace, 2) }} {{ stats.dist_abbr }} per day)
-    </li>
+    <div class="card my-2">
+        <div class="card-body">
+            {{ stats.name }}<br />
+            {{ formatDec(stats.total_dist, 2) }} {{ stats.dist_abbr }} in {{ stats.total_days }} days
+            <br />
+            <progress :value="stats.goal.current_distance_meters" :max="stats.goal.distance_meters" /><br />
+            {{ formatDec(stats.current_dist, 2) }} {{ stats.dist_abbr }} / {{ formatDec(stats.percent, 0) }}% complete
+            ({{ formatDec(Math.abs(stats.current_pace_diff), 2) }}
+            {{ stats.current_pace_diff >= 0 ? "over" : "under" }} pace)<br />
+            {{ stats.remaining_days }} days to go ({{ formatDec(stats.remaining_pace, 2) }} {{ stats.dist_abbr }} per
+            day)
+        </div>
+    </div>
 </template>
--- ui/src/components/GoalsList.vue
+++ ui/src/components/GoalsList.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import { useSession } from '../stores/session';
-import GoalView from './GoalView.vue';
+import { useSession } from "../stores/session";
+import GoalView from "./GoalView.vue";
 
 const session = useSession();
 </script>
 
 <template>
-    <ul>
-        <GoalView v-for="goal in session.goals" :key="goal.id" :goal="goal" />
-    </ul>
+    <GoalView v-for="goal in session.goals" :key="goal.id" :goal="goal" />
 </template>
