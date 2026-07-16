c5a9d5971a046f6bdef67c69e4ed2834fba02dc8
Joshua Foster <josh2112@gmail.com>
2026-07-16 17:33:18 -0400

    tiny stupid iterations on workflow, daemon & paths until i get something actually working

--- run-nfsn.sh
+++ run-nfsn.sh
@@ -1,1 +1,7 @@
-exec /home/protected/rungoal/api/.venv/bin/uvicorn rungoal.main:app --host 0.0.0.0 --port 8400 --proxy-headers --forwarded-allow-ips='*'
+exec /home/protected/rungoal/api/.venv/bin/uvicorn \
+	rungoal.main:app \
+	--host 127.0.0.1 \
+	--port 8400 \
+	--proxy-headers \
+	--forwarded-allow-ips='*' \
+	--root-path /rungoal
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,15 +1,16 @@
 import vue from "@vitejs/plugin-vue";
 import { readFileSync } from "fs";
 import { defineConfig } from "vite";
-import svgLoader from 'vite-svg-loader';
+import svgLoader from "vite-svg-loader";
 
-const packageJson = JSON.parse(readFileSync('package.json', 'utf-8'))
+const packageJson = JSON.parse(readFileSync("package.json", "utf-8"));
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue(), svgLoader()],
+    base: "/rungoal/",
     define: {
-        'import.meta.env.APP_VERSION': JSON.stringify(packageJson.version)
+        "import.meta.env.APP_VERSION": JSON.stringify(packageJson.version),
     },
     server: {
         proxy: {
@@ -17,11 +18,11 @@ export default defineConfig({
             // API requests are made to "/api". Vite catches those requests here and redirects
             // them to the backend server.
             //
-            // For production, FastAPI controls the flow through FastAPI.frontend() (see 
+            // For production, FastAPI controls the flow through FastAPI.frontend() (see
             // ../api/rungoal/main.py).
             "^/api/.*": {
                 target: "http://localhost:8000/",
-                changeOrigin: true
+                changeOrigin: true,
             },
         },
     },
@@ -29,12 +30,7 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                silenceDeprecations: [
-                    "if-function",
-                    "import",
-                    "color-functions",
-                    "global-builtin",
-                ],
+                silenceDeprecations: ["if-function", "import", "color-functions", "global-builtin"],
             },
         },
     },
