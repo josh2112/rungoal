a2c96e5fd0cc90f5ec932e55f1d77b890d513caf
Joshua Foster <josh2112@gmail.com>
2026-08-15 20:31:22 -0400

    it works!

--- api/rungoal/heatmap_test.py
+++ api/rungoal/heatmap_test.py
@@ -1,3 +1,5 @@
+import random
+import time
 from math import sqrt
 from typing import cast
 
@@ -28,32 +30,56 @@ def get_trackpoints():
 
 
 def make_blob():
-    img = Image.new("LA", (256, 256), 255)
+    img = Image.new("L", (256, 256), 255)
     if buf := img.load():
         for x in range(256):
             for y in range(256):
                 xd, yd = abs(128 - x), abs(128 - y)
-                buf[x, y] = (255, 256 - int(sqrt(xd * xd + yd * yd) * 2))
+                buf[x, y] = 256 - int(sqrt(xd * xd + yd * yd) * 2)
     img.save("assets/heatmap-point.png", "PNG")
 
 
-def try_heatmap():
-    pw, ph = 32, 32
-    img = Image.new("I", (48, 32), 0)
+def try_heatmap(img_size: tuple[int, int] = (256, 256), point_size: int = 32):
+    t = time.time()
+    pw, ph = point_size, point_size
+    img = Image.new("I", img_size, 0)
     point = Image.open("assets/heatmap-point.png").resize((pw, ph)).convert("I")
 
-    for x, y in ((16, 16), (32, 16)):
-        crop = img.crop((x - pw / 2, y - ph / 2, x + pw / 2, y + ph / 2))
+    pts = [
+        (
+            random.random() * (img_size[0] - pw) + pw / 2,
+            random.random() * (img_size[1] - ph) + ph / 2,
+        )
+        for _ in range(200)
+    ]
+
+    for x, y in pts:
+        x, y = int(x - pw / 2), int(y - ph / 2)
+        crop = img.crop((x, y, x + pw, y + ph))
         added_crop = ImageMath.unsafe_eval("A+B", A=crop, B=point)
         img.paste(added_crop, (x, y))
 
     vmax = cast(int, img.getextrema()[1])
     scale = 255.0 / vmax
-    print(scale)
 
-    img = img.point(lambda v: v * scale, mode="F")
-    print(img.getextrema()[1])
-    img.show()
+    img = img.point(lambda v: v * scale).convert(mode="L")
+    print("Elapsed", time.time() - t)
+    img.save("tmp.png", "PNG")
 
 
-try_heatmap()
+def recolor():
+    img = Image.open("tmp.png")
+    data = Image.open("assets/heatmap-lut.png").get_flattened_data()
+    r_channel = [pixel[0] for pixel in data]
+    g_channel = [pixel[1] for pixel in data]
+    b_channel = [pixel[2] for pixel in data]
+    a_channel = [pixel[3] for pixel in data]
+
+    correct_flat_lut = r_channel + g_channel + b_channel + a_channel
+
+    img = img.point(correct_flat_lut, mode="RGBA")
+    img.save("tmp2.png")
+
+
+# try_heatmap()
+recolor()
